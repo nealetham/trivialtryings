afd62d5e6d60a68fae9ac6194658d7c1fa7c951f
nealetham <nealetham2000@gmail.com>
2023-02-11 01:06:09 +0800

    Fix phone display

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,11 @@ export default function Home() {
   let toggleIcon;
 
   if (darkMode) {
-    toggleIcon = < BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl text-gray-900 dark:text-white'/>
+    toggleIcon = < BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-4xl text-gray-900 dark:text-white px-2 ml-4'/>
   } else {
-    toggleIcon = < BsFillSunFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl text-gray-900 dark:text-white'/>
+    toggleIcon = < BsFillSunFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-4xl text-gray-900 dark:text-white px-2 ml-4'/>
   }
+
   
   return (
     <div className={darkMode ? 'dark' : ''}>
@@ -29,20 +30,20 @@ export default function Home() {
       </Head>
       <main className="bg-white px-20 dark:bg-black content-center">
         <section className='min-h-screen' id='top'>
-          <nav className='p-10 mx-20 mb-12 flex justify-evenly'>
-            <a className='text-2xl font-burtons dark:text-white' href='#top'>trivialtryings</a>
-            <ul className='flex items-center'>
+          <nav className='p-10 mb-12 sm:flex flex-wrap justify-evenly'>
+            <a className='text-2xl font-burtons dark:text-white flex justify-center ' href='#top'>trivialtryings</a>
+            <ul className='flex justify-evenly'>
               <li>
                 {toggleIcon}
               </li>
               <li>
-                <a className='text-xl font-burtons text-gray-800 dark:text-white px-2 py-2 ml-4' href='#about'>About Me</a>
+                <a className='text-2xl font-burtons text-gray-800 dark:text-white pt-4 ml-4' href='#about'>about</a>
               </li>
               <li>
-                <a className='text-xl font-burtons text-gray-800 dark:text-white px-2 py-2 ml-4' href='#projects'>Projects</a>
+                <a className='text-2xl font-burtons text-gray-800 dark:text-white pt-4 ml-4' href='#projects'>projects</a>
               </li>
               <li>
-                <a className='text-xl font-burtons text-gray-800 dark:text-white px-2 py-2 ml-4' href='#'>Resume</a>
+                <a className='text-2xl font-burtons text-gray-800 dark:text-white pt-4 ml-4' href='#'>resume</a>
               </li>
               {/* <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
                       href='#'>Resume</a>
@@ -56,45 +57,56 @@ export default function Home() {
             <h3 className='text-xl py-4 leading-8 text-gray-800 dark:text-white'>I'm a 23 year old aspiring Software Engineer from Singapore, and <br/>I dabble in tech stuff.</h3>
           </div>
           <div className='flex justify-center flex-col items-center pt-14'>
-            <Image src={desktop} className='static z-10' alt={"desktop"}/>
-            <Image src={synth} className='relative bottom-96 -mt-16 z-0' width={280} height={250} alt={"vaporwave"} />
+            <Image src={desktop} className='static z-10' alt={"desktop"} />
+            {/* <div className='relative lg:bottom-96 lg:-mt-16 z-0 sm:bottom-96 sm:-mt-14 bottom-96 sm:h-72 sm:w-72 h-56 w-56'> */}
+            {/* <div className='relative bottom-80 -mt-4 h-52 w-52  sm:-mt-16 sm:bottom-96  sm:h-72 sm:w-72'> */}
+            <div className='relative bottom-56 h-36 w-36  sm:-mt-16 sm:bottom-96  sm:h-72 sm:w-72'>
+              <Image src={synth}  alt={"vaporwave"} />
+              {/* layout="fill" objectFit='cover' */}
+            </div>
           </div>
         </section>
 
         <section id="about">
           <h1 className='text-center text-4xl text-teal-500 mb-10'>About Me</h1>
-            <div className='container mx-auto px-60 dark:text-white'>
-              <div className='float-right rounded-full w-60 h-60 relative overflow-hidden md:h-60 md:w-60'>
-                <Image src={self} layout="fill" alt={"self-photo"} objectFit="cover" />
+            <div className='container mx-auto lg:px-60 dark:text-white'>
+              <div className='relative lg:float-right my-4 rounded-full overflow-hidden w-44 h-44 sm:w-60 sm:h-60 mx-auto'>
+                <Image src={self} layout="fill" alt={"self-photo"} objectFit="cover"/>
               </div>
-              <h2 className='text-2xl py-2 leading-8 text-gray-800 dark:text-white' >I'm a Year 2 Computer Science student at the National University of Singapore.</h2>
-              <p className='font-medium py-2 text-gray-800 dark:text-white' >I'm currently exploring my interests and have dipped my toes in 
-                Computer Networks, Database Systems and Computer Security. </p>
-              <p className='py-2 leading-8 text-gray-800 dark:text-white'>You might be wondering why the domain is named <span className='italic text-teal-500'>trivialtryings</span>. By definition,</p>
-              <blockquote className='indent-10 font-thin italic' cite="https://www.merriam-webster.com/thesaurus/trivial">
-                trivial - as in minor or lacking importance.
-              </blockquote>
-              <blockquote className='indent-10 font-thin italic' cite="https://www.merriam-webster.com/thesaurus/trying">
-                trying - as in hard or difficult to endure.
-              </blockquote>
-              <p className='py-2 leading-8 text-gray-800 dark:text-white'>I believe that trying new things should be trivial, and that trivial matters should not be trying.</p>
+
+              <div className='sm:p-10'>
+                <h2 className='text-2xl py-2 leading-8 text-gray-800 dark:text-white' >I'm a Year 2 Computer Science student at the National University of Singapore.</h2>
+                <p className='font-medium py-2 text-gray-800 dark:text-white' >I'm currently exploring my interests and have dipped my toes in 
+                  Computer Networks, Database Systems and Computer Security. </p>
+                <p className='py-2 text-gray-800 dark:text-white'>You might be wondering why the domain is named <span className='italic text-teal-500'>trivialtryings</span>. By definition,</p>
+                <blockquote className='indent-10 font-thin italic py-2' cite="https://www.merriam-webster.com/thesaurus/trivial">
+                  trivial - as in minor or lacking importance.
+                </blockquote>
+                <blockquote className='indent-10 font-thin italic py-2' cite="https://www.merriam-webster.com/thesaurus/trying">
+                  trying - as in hard or difficult to endure.
+                </blockquote>
+                <p className='py-2 text-gray-800 dark:text-white'>I believe that trying new things should be trivial, and that trivial matters should not be trying.</p>
+              </div>
+
+
+              <div className='container sm:px-10 sm:pt-0 pt-5 text-3xl flex justify-start gap-16 text-gray-900'>
+                <Link href="https://www.linkedin.com/in/nealetham" target="_blank" rel="noopener noreferrer">
+                  <AiFillLinkedin className="cursor-pointer dark:text-white" />
+                </Link>
+                <Link href="https://github.com/nealetham" target="_blank">
+                  <AiFillGithub className="cursor-pointer dark:text-white" />
+                </Link> 
+                <Link href="mailto:neale.tgc@gmail.com" target="_blank">
+                  <AiFillMail className="cursor-pointer dark:text-white"/>
+                </Link>
+              </div>
 
             </div>
+
+
+
         </section>
 
-        <section>
-          <div className='container mx-auto px-60 text-3xl flex justify-start gap-16 text-gray-900 m-5'>
-            <Link href="https://www.linkedin.com/in/nealetham" target="_blank" rel="noopener noreferrer">
-                <AiFillLinkedin className="cursor-pointer dark:text-white" />
-              </Link>
-              <Link href="https://github.com/nealetham" target="_blank">
-                <AiFillGithub className="cursor-pointer dark:text-white" />
-              </Link> 
-            <Link href="mailto:neale.tgc@gmail.com" target="_blank">
-              <AiFillMail className="cursor-pointer dark:text-white"/>
-              </Link>
-          </div>
-        </section>
 
         <section id="projects">
           <h1 className='text-center text-4xl text-teal-500 mt-40'>Projects</h1>
